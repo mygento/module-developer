61e53c1a1278541c1335bd97ecd33293e3854b3b
nikita tarasov <nikita@mygento.ru>
2019-04-02 18:09:25 +0300

    init 2.3.0

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author Mygento Team
+ * @copyright 2018-2019 Mygento (https://www.mygento.ru)
+ * @package Mygento_Developer
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Mygento_Developer',
+    __DIR__
+);
