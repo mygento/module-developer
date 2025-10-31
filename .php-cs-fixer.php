0d1caf2aebca3deeacc143475199f0d1b4a900b5
Nikita <nikita@MacBookPro.fritz.box>
2025-10-31 17:12:12 +0100

    modernize

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,7 +1,7 @@
 <?php
 $header = <<<EOF
 @author Mygento Team
-@copyright 2018-2022 Mygento (https://www.mygento.com)
+@copyright 2018-2026 Mygento (https://www.mygento.com)
 @package Mygento_Developer
 EOF;
 
--- registration.php
+++ registration.php
@@ -2,12 +2,12 @@
 
 /**
  * @author Mygento Team
- * @copyright 2018-2022 Mygento (https://www.mygento.com)
+ * @copyright 2018-2026 Mygento (https://www.mygento.com)
  * @package Mygento_Developer
  */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Mygento_Developer',
-    __DIR__
+    __DIR__,
 );
